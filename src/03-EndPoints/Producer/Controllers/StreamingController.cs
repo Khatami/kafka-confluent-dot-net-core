18d8a8b5badd6d10fa2cfbc4fe724538a7be44a4
hamedkhatami <hamedkhatami@outlook.com>
2023-12-23 14:08:22 +0330

    feat: producer

---
+++ src/03-EndPoints/Producer/Controllers/StreamingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streaming.Contracts;
+
+namespace Producer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StreamingController : ControllerBase
+	{
+		private readonly IStreamingService _streamingService;
+
+		public StreamingController(IStreamingService streamingService)
+		{
+			_streamingService = streamingService;
+		}
+
+		[HttpPost]
+		public IActionResult Produce()
+		{
+			_streamingService.Publish("test", typeof(TestEvent).ToString(), new TestEvent() { Id = 1, Name = "Hamed Khatami" });
+
+			return Ok();
+		}
+	}
+
+	public class TestEvent
+	{
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
